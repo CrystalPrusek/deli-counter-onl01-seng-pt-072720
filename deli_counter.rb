43835af6885765a7db123842d9ad9d150a5d3c92
Crystal Prusek <crystal.prusek@gmail.com>
2020-08-07 22:02:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,18 +25,13 @@ def line_simple(array)
 end 
   
 line_simple(katz_deli)  
-#[:foo, :bar, :baz].each.with_index(1) do |value, index|
-#    puts "#{index}: #{value}"
-#end
-
-
 
 def take_a_number(line, new_person)
-  line.push(new_person) # could say: "line << new_person"
+  line.push(new_person) 
   puts "Welcome, #{new_person}. You are number #{line.length} in line."
 end
 
-take_a_number(katz_deli, "Fyvish")
+take_a_number(katz_deli)
 
 def now_serving(line)
   if line.length == 0 # could say: "if deli.empty?"
